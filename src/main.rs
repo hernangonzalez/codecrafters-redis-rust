dcbf9ad1a5a1f108b6bfc7dea0b218c71a373c8a
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-03-09 15:51:20 +0000

    redis pong w/o crates (2)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use anyhow::*;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -10,22 +11,23 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
     println!("Listening at 6379...");
 
-    let (_stream, origin) = listener.accept().await?;
-    println!("Firs stream... {origin}");
+    loop {
+        let (mut stream, origin) = listener.accept().await?;
+        println!("Client connected from: {origin}");
 
-    Ok(())
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+        let mut buffer = [0u8; 1024];
+        let read_size = stream.read(&mut buffer).await?;
+        if read_size == 0 {
+            println!("Empty message, shutting down");
+            stream.shutdown().await?
+        }
+
+        println!("Received message: {}", String::from_utf8_lossy(&buffer));
+        let response = match &buffer[..] {
+            b"PING" => "+PONG\r\n",
+            _ => "+PONG\r\n",
+        };
+
+        stream.write_all(response.as_bytes()).await?;
+    }
 }
