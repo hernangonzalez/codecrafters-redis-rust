f95516e73c3b1ce3be4bf32802aa3816cd9e75dd
Hernan G. Gonzalez <camus.sonico@gmail.com>
2023-12-16 20:03:38 -0300

    fix spawn script

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -9,4 +9,6 @@ exec cargo run \
     --quiet \
     --release \
     --target-dir=/tmp/codecrafters-redis-target \
-    --manifest-path $(dirname $0)/Cargo.toml "$@"
+    --manifest-path $(dirname $0)/Cargo.toml \
+    -- "$@"
+    
